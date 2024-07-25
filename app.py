08c866db7c32d251a558732a6d6f44b031c89b97
Xavi Omar <egyptiango17@gmail.com>
2024-07-25 22:36:14 +0300

    update the dobroje database through http

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import os
 import sys
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-from typing import Dict, Any
-from typing import List
+from typing import Dict, Any, List
+import threading
+import time
+import requests
+from dotenv import load_dotenv
+import json
+import http.client
 
 # Add the project directory to the system path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -12,6 +17,14 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config.settings import *
 from src.tuya_cloud_connect import TuyaDeviceManager
 from src.database import MongoDBClient
+
+# Load environment variables from a .env file
+load_dotenv()
+
+# Retrieve API credentials from environment variables
+API_REGION = os.getenv('API_REGION')
+API_KEY = os.getenv('API_KEY')
+API_SECRET = os.getenv('API_SECRET')
 
 app = FastAPI(
     title="Tuya Device API",
@@ -30,7 +43,6 @@ class MultiSensorDataRequest(BaseModel):
     API_KEY: str = Field(..., description="API key for Tuya", example="9u7uqwsp7u9pxkfmswae")
     API_SECRET: str = Field(..., description="API secret for Tuya", example="5479b5a39e464313911b4a41eb0c7355")
     DEVICE_ID: List[str] = Field(..., description="Device IDs of the Tuya devices", example=["vdevo172044590691636", "vdevo172044570814122"])
-
 
 class CommandRequest(BaseModel):
     API_REGION: str = Field(..., description="API region for Tuya", example="eu")
@@ -115,7 +127,104 @@ async def send_command(request: CommandRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+def is_data_changed(db_client, device_id, new_status):
+    """
+    Check if the new data differs from the latest data in the database.
+    
+    Args:
+    db_client: Database client instance.
+    device_id: ID of the device.
+    new_status: New data from the sensor.
+    
+    Returns:
+    bool: True if data has changed, False otherwise.
+    """
+    try:
+        latest_record = db_client.get_latest_record(device_id)
+        return latest_record['result'] != new_status['result']
+    except Exception as e:
+        print(f"Error checking data change: {e}")
+        return True  # Assume data has changed if there's an error
+
+def send_data_to_endpoint(device_id, data):
+    """
+    Send data to a specific endpoint.
+    
+    Args:
+    device_id: ID of the device.
+    data: Data to be sent.
+    """
+    conn = http.client.HTTPConnection("dobroje.rs")
+    
+    payload = json.dumps({
+        "deviceID": device_id,
+        "data": data
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    try:
+        conn.request("POST", "/parametri.php?action=tuyapayload", payload, headers)
+        response = conn.getresponse()
+        
+        if response.status == 200:
+            print(f"Successfully sent data for device: {device_id}")
+        else:
+            print(f"Failed to send data for device: {device_id}, status code: {response.status}")
+            response_content = response.read().decode()
+            print(response_content)
+            
+    except Exception as e:
+        print(f"Error sending data to endpoint: {e}")
+    finally:
+        conn.close()
+
+def database_update(db_client, device):
+    """
+    Continuously update the database with new device data if it has changed.
+    
+    Args:
+    db_client: Database client instance.
+    device: Device instance.
+    """
+    while True:
+        try:
+            # Retrieve status from the sensors
+            device_status = device.get_status()
+            # Check if the luminance data has changed and update the database if it has
+            if is_data_changed(db_client, device.device_id, device_status):
+                db_client.insert_data(device.device_id, device_status)
+                print(f"Inserted new data for device: {device.device_id}: {device_status}")
+                send_data_to_endpoint(device.device_id, device_status)
+
+            # Wait for 1 second before the next iteration
+            time.sleep(1)
+
+        except Exception as e:
+            print(f"Error in database update: {e}")
 
 if __name__ == "__main__":
     import uvicorn
+
+    db_client = MongoDBClient()
+    
+    # Initialize TuyaDeviceManager for each device
+    devices = [
+        TuyaDeviceManager(API_REGION, API_KEY, API_SECRET, DEVICES['Irrigation controller']),
+        TuyaDeviceManager(API_REGION, API_KEY, API_SECRET, DEVICES['Luminance sensor']),
+        TuyaDeviceManager(API_REGION, API_KEY, API_SECRET, DEVICES['Smart soil sensor'])
+    ]
+
+    # Create threads for concurrent execution of database update for each device
+    threads = []
+    for device in devices:
+        thread = threading.Thread(target=database_update, args=(db_client, device))
+        threads.append(thread)
+        thread.start()
+
+    # Join threads to ensure the main program waits for them to finish
+    for thread in threads:
+        thread.join()
+    
     uvicorn.run(app, host="0.0.0.0", port=8000)
